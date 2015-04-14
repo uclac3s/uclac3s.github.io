1336512aa5b406a6ec822cdbe3bdf948a9675d3e
victor2100 <fudanvictor@gmail.com>
2015-04-14 15:08:45 -0700

    compress img

--- compress.sh
+++ compress.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 folder=$(pwd)
-p=$folder/img/event1
+#p=$folder/img/event1
+#p=$folder/members
+p=$folder/advisors
 cd $p
 
 for img in *.jpg; do
     #convert -quality 75 "$img" converted/${name%.jpg}.jpg
     if [ ! -e $folder/compressed/"$img" ]
     then
-	echo "new added: $img"
-    	convert -quality 75 -resize 50% "$img" $folder/compressed/"$img"
+	RESULTS_SIZE=`stat -c %s $img`
+	if [ "$RESULTS_SIZE" -gt 102400 ]
+	then
+		echo "new added: $img"
+		#convert -quality 75 "$img" $folder/compressed/"$img"
+    		convert -quality 75 -resize 50% "$img" $folder/compressed/"$img"
+	fi
     fi
 done
 
