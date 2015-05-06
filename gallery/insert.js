97d8234dd95a11f26f4cc2fbb9d93d4ef0d77ae1
victor2100 <fudanvictor@gmail.com>
2015-05-06 13:46:27 -0700

    add new event

--- gallery/insert.js
+++ gallery/insert.js
@@ -1,9 +1,13 @@
 //$(document).ready(function(){
 // javascript placed here to mimic html insertion
 // var photo_lst = ['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
+
+// ===== Change it to reflect the max photo index =====
 var maxIndex = 10;
+
+// ===== Don't change anything below =====
 for (var i=0; i<=maxIndex; i++) {
-	var fname = 'gallery/' + i +'.jpg'
+	var fname = 'gallery/' + i +'.jpg';
 	$('<div class="item"><a href="' + fname + '" title="" data-lightbox-gallery="gallery1" data-lightbox-hidpi=""><img src="' +
 		fname + '" class="img-responsive " alt="img"></a></div>').insertBefore(".photo-anchor");
 }
