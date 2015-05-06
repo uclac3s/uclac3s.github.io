9ae319033a333e0923afd44938d52e18b9dc5e85
victor2100 <fudanvictor@gmail.com>
2015-05-06 13:30:11 -0700

    more easy add new photos to gallery

---
+++ gallery/insert.js
@@ -0,0 +1,11 @@
+//$(document).ready(function(){
+// javascript placed here to mimic html insertion
+// var photo_lst = ['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
+var maxIndex = 10;
+for (var i=0; i<=maxIndex; i++) {
+	var fname = 'gallery/' + i +'.jpg'
+	$('<div class="item"><a href="' + fname + '" title="" data-lightbox-gallery="gallery1" data-lightbox-hidpi=""><img src="' +
+		fname + '" class="img-responsive " alt="img"></a></div>').insertBefore(".photo-anchor");
+}
+$(".photo-anchor").remove();
+//});
