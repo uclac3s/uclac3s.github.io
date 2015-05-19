d8d9d410421e3f35aab609fab34bd9a68b6ae4da
victor2100 <fudanvictor@gmail.com>
2015-05-18 20:55:25 -0700

    add new calendar

---
+++ calendar_data.js
@@ -0,0 +1,51 @@
+var c_data =
+[
+	{
+		"title": "Can we Design \n Our Career",
+		"start": "2015-04-10"
+	},
+	{
+		"title": "How to Present \n Your Research",
+		"start": "2015-04-21"
+	},
+	{
+		"title": "How to Build a Successful \n Career in US",
+		"start": "2015-05-14"
+	}
+/*
+  {
+	"title": "All Day Event T",
+	"start": "2015-05-02"
+  },
+  {
+	"title": "Long Event",
+	"start": "2015-06-07",
+	"end": "2015-06-10"
+  },
+  {
+	"title": "Repeating Event",
+	"start": "2015-06-09T16:00:00"
+  },
+  {
+	"title": "Repeating Event",
+	"start": "2015-06-16T16:00:00"
+  },
+  {
+	"title": "Meeting",
+	"start": "2015-06-12T10:30:00",
+	"end": "2015-06-12T12:30:00"
+  },
+  {
+	"title": "Lunch",
+	"start": "2015-06-12T12:00:00"
+  },
+  {
+	"title": "Birthday Party",
+	"start": "2015-06-13T07:00:00"
+  },
+  {
+	"title": "Click for Google",
+	"url": "http://google.com/",
+	"start": "2015-06-28"
+  }*/
+];
