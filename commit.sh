691b9717738e8a6ca6210be0aa8d318dad316e4f
frog <Victor@mac.local>
2015-11-06 01:33:50 -0800

    add html preprocessor

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd DIR
+
+echo "[Main] process html pages"
+python html_preprocessor.py
+
+echo "[Main] git commit and push"
+git add .
+git commit -m "auto update"
+git push
---
+++ html_preprocessor.py
@@ -0,0 +1,88 @@
+
+lines = []
+lineIndex = 0
+htmlFile = 'index.html'
+info = '<!-- Generated from ' + htmlFile + '.template. Do not edit. -->'
+
+def output():
+	with open(htmlFile+'.template') as fin:
+		with open(htmlFile, 'w') as fout:
+			fout.write(info+'\n')
+			for l in fin.readlines():
+				replaceLine = False
+				ls = l.strip()
+				for key in func_map:
+					# replace the template line
+					if ls.startswith('<!--') and (key + ' content') in ls:
+						fout.write('\n'.join(resultSet[key])+'\n')
+						replaceLine = True
+						break
+				if not replaceLine:
+					fout.write(l)
+
+def read():
+	for l in open('data.txt').readlines():
+		l = l.strip()
+		if len(l) > 0 and not l.startswith('#'):
+			lines.append(l)
+
+def nextLine():
+	global lineIndex
+	if lineIndex < len(lines):
+		lineIndex += 1
+		return lines[lineIndex-1]
+	else:
+		return None
+
+def process():
+	func = None
+	while True:
+		line = nextLine()
+		controlLine = False
+		if line == None:
+			break
+		for key in func_map:
+			if '[' + key + ']' == line:
+				func = func_map[key]()
+				controlLine = True
+		if not controlLine:
+			func(line)
+
+def mode1():
+	def doit(line):
+		date = line
+		title = nextLine()
+		noteLink = nextLine()
+		resultSet['events'].append('<div><div class="col-md-9"><a class="titleLink" href="events/%s.jpg" target="_blank">%s</a></div><div class="col-md-2"><a class="noteLink" href="%s" target="_blank">(Notes for %s)</a></div></div>'\
+			% (date.replace('.', '_'), title, noteLink, date))
+	return doit
+
+def mode2():
+	def doit(line):
+		title = line
+		link = nextLine()
+		resultSet['westwood cafe'].append('<div class="col-md-12 marginbot-20"><a class="storyLink" href="' + link +'">' + title + '</a></div>')
+	return doit
+
+def mode3():
+	def doit(line):
+		title = line
+		link = nextLine()
+		resultSet['story'].append('<div class="col-md-12 marginbot-20"><a class="storyLink" href="' + link +'">' + title + '</a></div>')
+	return doit
+
+func_map = {
+	'events': mode1,
+	'westwood cafe': mode2,
+	'story': mode3
+}
+
+resultSet = {key: [] for key in func_map}
+
+def main():
+	read()
+	process()
+	output()
+
+### TODO: automatic detect photo and add them into html page
+main()
