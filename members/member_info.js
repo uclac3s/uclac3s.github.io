4c7f94234e48fff55f47224b916f7307ca699487
lyuanqi <liyuanqi@Yuanqis-MacBook-Pro-2.local>
2017-01-02 17:45:24 +0800

    updated the member info and pics

---
+++ members/member_info.js
@@ -0,0 +1,186 @@
+var data = function () {/*
+###
+### comments starts with #
+### please update this form as the existing format
+###
+
+[template:leader_info]
+
+# image_title.jpg
+# First_Name Last_Name
+# Current_Position_At_C3S
+# Current_Title
+# Major
+# Research_Interest/Career_Interest
+
+李睿.jpg
+李睿 | Rui Li
+VP
+Post-doc Scholar
+Molecular and Medical Phamarcology
+Cancer Metastasis, Tumor Microenvironment
+Zhejiang University
+
+杨欢.jpg
+杨欢 | Huan Yang
+President
+Ph.D Candidate
+Molecular, Celluar & Integrative Physiology
+Molecular pathogenesis of neurodegeneration; mitochondrial biology
+Zhejiang University
+
+王禾.jpg
+王禾 | He Wang
+VP
+Ph.D Candidate
+Molecular, Celluar & Integrative Physiology
+Cardiac hypertropgy; heart failure; epigenetics
+Xi'an Jiaotong University
+
+方天成.jpg
+方天成 | Tiancheng Fang
+Cochair, Project Management/Marketing Department
+Ph.D Candidate
+Molecular and Medical Phamarcology
+Hematopoietic stem cell and bone marrow transplant
+Nankai University
+
+傅凯.jpg
+傅凯 | Kai Fu
+Cochair, Project Management/Marketing Department
+Ph.D Candidate
+Bioinformatics
+Personalized medicine, molecular diagnosis, epigenetics
+Tongji University
+
+王莹.jpg
+王莹 | Ying Wang
+Chair, Human Resources/Internal Affairs Department
+Ph.D Candidate
+Molecular, Celluar & Integrative Physiology
+muscle aging, macrophage phenotype and function
+Nankai University
+
+杨松.jpg
+杨松 | Song Yang
+Chair, Finance/Internal Affairs Department
+Ph.D Candidate
+Chemistry
+Molecular dynamics of enzymes and molecular machines
+Fudan University
+
+王路岚.jpg
+王路岚 | Lulan Wang
+Graphic Designer, Photographer
+Ph.D Candidate
+Immunity, Microbes & Molecular Pathogenesis
+Influenza virus, Zika virus
+N/A
+
+顾家奇.jpg
+顾家奇 | Jiaqi Gu
+Chair, IT Department
+Ph.D Candidate
+Computer Science
+Big Data Systems
+Fudan University
+
+[template:member_info]
+
+王嘉瑞.jpg
+王嘉瑞 | Jiarui Wang
+Project Management/Marketing Department
+Junior
+Business Econ
+Finance/Marketing
+N/A
+
+钟杉.jpg
+钟杉 | Shan Zhong
+Media/Communications Department
+Freshmen
+Applied Math/ Statistics
+Database Development/Big Data
+N/A
+
+郑雅琪.jpg
+郑雅琪 | Yinglin Zheng
+Project Management/Marketing Department
+Junior
+Business Econ
+Finance/Eutrepreneurship/Management
+N/A
+
+李文渊.jpg
+李文渊 | Wenyuan Li
+Project Management/Marketing Department
+Ph.D Candidate
+Electrical Engineering
+Neuromorphic Computing/Artifiicial Intelligence/Data Science
+Zhejiang University
+
+胡峻珲.jpg
+胡峻珲 | Junhui Hu
+IT Department
+Visiting Scholar
+Molecular and Medical Phamarcology
+Cancer Metastasis
+N/A
+
+翟瑞文.jpg
+翟瑞文 | Ruiwen Zhai (Crystal)
+Media/Communications Department
+Junior
+Chemical Engineering
+Semiconductor/New energy
+N/A
+
+黎皓然.jpg
+黎皓然 | Haoran Li
+Media/Communications Department
+Junior
+Sociology/ Global Studies
+Western Marxism/ Social Work/ Minority Stress Model
+N/A
+
+周正韡.jpg
+周正韡 | Zhengwei Zhou (Jenny)
+Media/Communications Department
+Ph.D Candidate
+Department of Bioengineering
+Medical Imaging, MRI
+N/A
+
+李浏博.jpg
+李浏博 | Liubo Li
+Media/Communications Department
+Ph.D Student
+Organic Chemistry
+Medicinal Chemistry and Total Synthesis
+N/A
+
+沈阳.jpg
+沈阳 | Yang Shen
+Media/Communications Department
+Ph.D Candidate
+Mechanical Engineering
+Exoskeleton Robot for Stroke Rehabilitation, Human Computer Interaction, Intelligent Control
+B.Eng. The University of Hong Kong, B.S. Shanghai Jiao Tong University (2+2)
+
+严霄.jpg
+严霄 | Xiao Yan
+Project Management/Marketing Department
+N/A
+Molecular and Medical Phamarcology
+Hematopoietic Stem Cell Regeneration and Drug Development
+Peking University
+
+喻宁.jpg
+喻宁 | Ning Yu
+Project Management/Marketing Department
+N/A
+N/A
+N/A
+Tsinghua University
+
+*/};
